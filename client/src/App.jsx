a9c6da2afb055be35290c5b43baa6b89d597c6f5
Sanan <sananahmad.dev@gmail.com>
2024-09-24 23:12:33 +0500

    Added the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header';
-import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import News from "./components/News";
+// import Footer from "./components/Footer";
+import TopHeadlines from "./components/TopHeadlines";
 
-export function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CountryNews from "./components/CountryNews";
+
+function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='w-full'>
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </div>
-    </>
-  )
+    <div className="w-full">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<News />} />
+          <Route path="/top-headlines/:category" element={<TopHeadlines />} />
+          <Route path="/country/:iso" element={<CountryNews />} />
+        </Routes>
+        {/* <Cards />  */}
+        {/* <Footer />   */}
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     screens: {
-      sm: "576px",
-      // => @media (min-width: 576px) { ... }
+      xs: { max: "640px" },
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
-      md: "960px",
-      // => @media (min-width: 960px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
 
-      lg: "1440px",
-      // => @media (min-width: 1440px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
     colors: {
       lightBg: "#E2E8F0",
       lightBgPrimary: "#F8FAFC",
-      lightBgSecondary: "0F172A",
+      lightBgSecondary: "#0F172A",
     },
   },
-
   plugins: [],
 };
